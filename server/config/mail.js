848e2eb2d4540c6435cd96c9622ae1388e1cc3ea
Carlos <carlosandre116@gmail.com>
2022-09-26 17:18:14 -0300

    Finalização Serviço de E-mail Avaliado 26.09.2022

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import 'dotenv/config';
 
 async function mailConfig() {
   const config = {
--- server/services/SendMail.js
+++ server/services/SendMail.js
@@ -11,13 +11,11 @@ async function createNewUser(to) {
 		const htmlContent = fs.readFileSync('server/models/emails/newUser.html',
             {encoding:'utf8'});
 
-		console.log(htmlContent)
-
     const info = await transporter.sendMail({
       from: 'noreplay@email.com',
       to,
       subject: 'Conta criada no Currículo Maker',
-      text: `Conta criada com sucesso.\n\nAcesse o aplicativo para gerenciar o cadastro de comidas.`,
+      text: `Conta criada com sucesso.\n\nAcesse o aplicativo para gerenciar o cadastro de currículos.`,
       html: `${htmlContent}`,
     });
 
@@ -32,8 +30,6 @@ async function sendNewCurriculum(to, htmlContent) {
     const config = await mailConfig();
 
     const transporter = nodemailer.createTransport(config);
-
-		console.log(htmlContent)
 
     const info = await transporter.sendMail({
       from: 'noreplay@email.com',
